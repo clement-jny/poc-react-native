4b4f738546a7754786a642f8398ef1c7b79cf024
clement-jny <clementjaunay@free.fr>
2024-10-14 12:48:37 +0200

    try inline style

--- App.tsx
+++ App.tsx
@@ -4,34 +4,44 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState<number>(0);
 
-  const increment = () => {
-	setCount(count => count + 1);
-  };
+	const increment = () => {
+		setCount(count => count + 1);
+	};
 
-  const decrement = () => {
-	if (count > 0) {
-		setCount(count => count - 1);
-	}
-  };
+	const decrement = () => {
+		if (count > 0) {
+			setCount(count => count - 1);
+		}
+	};
 
-  return (
-	<View style={styles.container}>
-		<Button title='-' onPress={decrement} />
-		<Text>{count}</Text>
-		<Button title='+' onPress={increment} />
-	</View>
-  );
+	return (
+		<View style={{ display: 'flex', justifyContent: 'space-between', padding: 60, flex: 1 }}>
+			<View style={{ backgroundColor: 'green', padding: 20 }}>
+				<Text style={{ fontSize: 20, textAlign: 'center' }}>Header</Text>
+			</View>
+
+			<View style={{ backgroundColor: 'red', padding: 20 }}>
+				<Text style={{ fontSize: 20, textAlign: 'center' }}>Footer</Text>
+			</View>
+		</View>
+
+		// <View style={styles.container}>
+		// 	<Button title='-' onPress={decrement} />
+		// 	<Text>{count}</Text>
+		// 	<Button title='+' onPress={increment} />
+		// </View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-	height: 100,
-	width: 100,
-	flex: 1,
-	flexDirection: 'row',
-	justifyContent: 'center',
-    alignItems: 'center'
-  },
+	container: {
+		height: 100,
+		width: 100,
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
 });

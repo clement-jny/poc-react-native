e08ca2068ae7069c476bbeddfd2a4d99045bac22
clement-jny <clementjaunay@free.fr>
2024-10-14 12:51:56 +0200

    do with StyleSheet

--- App.tsx
+++ App.tsx
@@ -4,39 +4,43 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-	const [count, setCount] = useState<number>(0);
-
-	const increment = () => {
-		setCount(count => count + 1);
-	};
-
-	const decrement = () => {
-		if (count > 0) {
-			setCount(count => count - 1);
-		}
-	};
-
 	return (
-		<View style={{ display: 'flex', justifyContent: 'space-between', padding: 60, flex: 1 }}>
-			<View style={{ backgroundColor: 'green', padding: 20 }}>
-				<Text style={{ fontSize: 20, textAlign: 'center' }}>Header</Text>
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.text}>Header</Text>
 			</View>
 
-			<View style={{ backgroundColor: 'red', padding: 20 }}>
-				<Text style={{ fontSize: 20, textAlign: 'center' }}>Footer</Text>
+			<View style={styles.footer}>
+				<Text style={styles.text}>Footer</Text>
 			</View>
 		</View>
-
-		// <View style={styles.container}>
-		// 	<Button title='-' onPress={decrement} />
-		// 	<Text>{count}</Text>
-		// 	<Button title='+' onPress={increment} />
-		// </View>
 	);
 }
 
+// const CounterView = () => {
+// 	const [count, setCount] = useState<number>(0);
+
+// 	const increment = () => {
+// 		setCount(count => count + 1);
+// 	};
+
+// 	const decrement = () => {
+// 		if (count > 0) {
+// 			setCount(count => count - 1);
+// 		}
+// 	};
+
+// 	return (
+// 		<View style={styles.countContainer}>
+// 			<Button title='-' onPress={decrement} />
+// 			<Text>{count}</Text>
+// 			<Button title='+' onPress={increment} />
+// 		</View>
+// 	);
+// }
+
 const styles = StyleSheet.create({
-	container: {
+	countContainer: {
 		height: 100,
 		width: 100,
 		flex: 1,
@@ -44,4 +48,25 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
+
+	container: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		padding: 60,
+		flex: 1
+	},
+
+	header: {
+		backgroundColor: 'green',
+		padding: 20
+	},
+	footer: {
+		backgroundColor: 'red',
+		padding: 20
+	},
+
+	text: {
+		fontSize: 20,
+		textAlign: 'center'
+	}
 });
